d51b0ee2786af5a7794b7a9bc5c1cc7ced0999b0
Brayden Petersen <115945971+braydenpetersen@users.noreply.github.com>
2025-11-25 23:45:00 -0500

    add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,25 @@ import PageTransition from "@/components/PageTransition";
 export const metadata: Metadata = {
   title: "Design Waterloo",
   description: "Design Waterloo is a collective and directory of student designers, artists, filmmakers, engineers, and creatives from the University of Waterloo and Wilfrid Laurier University.",
+  openGraph: {
+    title: "Design Waterloo",
+    description: "Design Waterloo is a collective and directory of student designers, artists, filmmakers, engineers, and creatives from the University of Waterloo and Wilfrid Laurier University.",
+    images: [
+      {
+        url: '/ogimage.png',
+        width: 1200,
+        height: 630,
+        alt: 'Design Waterloo',
+      }
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Design Waterloo",
+    description: "Design Waterloo is a collective and directory of student designers, artists, filmmakers, engineers, and creatives from the University of Waterloo and Wilfrid Laurier University.",
+    images: ['/ogimage.png'],
+  },
 };
 
 export const viewport: Viewport = {
